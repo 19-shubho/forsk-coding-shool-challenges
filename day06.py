84abf7da288dde1c4e07e52a2ea619f67bd22c0e
19-shubho <53344823+19-shubho@users.noreply.github.com>
2021-09-02 21:35:41 +0530

    Add files via upload
    list operations and dictionary

---
+++ day06.py
@@ -0,0 +1,60 @@
+list=[9,7,0,53,76,1]
+type(list)
+list.append(99)
+list[1]=12
+#lists are mutable unlike strings
+list.insert(2,8)
+list.remove(8)
+list.count(0)
+list.insert(4,7)
+list.insert(2,9)
+list.insert(3,7)
+list.insert (5,5) 
+list.insert(8,0)
+list.insert(6,1)
+
+list0=['AA', 'BB', 'JJ']
+for i in list0:
+    print(i)
+    
+lt=[1,11,121,11211]
+li=[]
+for i in lt:
+    li.append(i*i)
+    
+    
+for i in list0:
+    li.append(len(i))    
+    
+lo=[len(i) for i in list0]   
+    
+[i*i/2 for i in lt]
+    
+tesher={
+ 'name':'Asdfwqer',
+ 'address': '52 jung street, skyhigh, barapola',
+ 'roll': 67,
+ 'sst': 88,
+ 'eco': 98,
+ 'sport': 'swimming'
+ }
+
+for x in tesher:
+    print (x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
