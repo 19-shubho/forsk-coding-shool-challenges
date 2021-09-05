ca4acdf6cddf0940cf6a4994d6cb4e68d4c24ed7
Shubhodeep Roy <53344823+19-shubho@users.noreply.github.com>
2021-09-06 00:43:03 +0530

    Add files via upload
    mapping value of y using linear regression..................to be continued on later days

---
+++ day31.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+dataset = pd.read_csv("student_scores.csv")
+
+dataset.shape
+dataset.columns
+
+dataset.isnull().any(axis = 0)
+
+
+features = dataset['Hours'].values
+
+labels = dataset['Scores'].values
+
+
+import matplotlib.pyplot as plt
+plt.scatter(features,labels)
+
